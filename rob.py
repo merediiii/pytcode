5c1e1009ae40e5068ccb04c67313e63659b81c18
Merediii <realmadridivy@126.com>
2019-09-06 13:40:22 +0800

    198. 打家劫舍

--- rob.py
+++ rob.py
@@ -6,7 +6,7 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 0:return 0
         if len(nums) == 1:return nums[0]
-        r = [nums[1],max(nums[0],nums[1])]
+        r = [nums[0],max(nums[0],nums[1])]
         for i in range(2,len(nums)):
             r.append(max(r[i-2] + nums[i],r[i-1]))
         return r[len(nums)-1]
