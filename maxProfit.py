332a45909d9813c002078e471f6b19490a1dda54
Merediii <realmadridivy@126.com>
2019-09-06 13:36:22 +0800

    121. 买卖股票的最佳时机

---
+++ maxProfit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        r = 0
+        a = 0x3f3f3f3f
+        for i in range(0,len(prices)):
+            a = min(a,prices[i])
+            r = max(r, prices[i] - a)
+        return r
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.c([7,1,5,3,6,4]))
